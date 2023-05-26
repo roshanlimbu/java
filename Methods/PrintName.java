d66af9d49d81840cbd615d359a7367065c67e6ff
Roshan Limbu <topazkayle@gmail.com>
2023-05-26 14:38:05 +0545

    Assignment 3 ques1

---
+++ Methods/PrintName.java
@@ -0,0 +1,10 @@
+// Write a program in Java to create and print your name using the method.
+public class PrintName{
+  public static void main(String[]args){
+    NewFunc(); // calling function
+  }
+  static void NewFunc(){ // declaring the function here 
+   System.out.println("Roshan"); // message to print
+}
+}
+
