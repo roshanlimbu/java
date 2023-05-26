b1dafc35bc234eff527f85f77124bed747fada27
Roshan Limbu <topazkayle@gmail.com>
2023-05-26 09:02:26 +0545

    '....'

---
+++ javaPractice/Round.java
@@ -0,0 +1,7 @@
+public class Round{
+  public static void main(String[]args){
+    float Fl=2.356452f;
+    int Rounded = Math.round(Fl);
+    System.out.println(Rounded);
+  }
+}
--- javaPractice/Vowel.java
+++ javaPractice/Vowel.java
@@ -2,6 +2,10 @@
 import java.util.Scanner;
 public class Vowel{
   public static void main(String[]args){
-    
-  }
+    Scanner inp= new Scanner(System.in);
+    System.out.println("Enter a word.");
+    char letter = inp.next().charAt(0);
+    letter = Character.toLowerCase(letter);
+    System.out.println(letter);
+    }
 }
