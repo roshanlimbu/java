a5cab13e7262bfc4a7562602942a2c5305a8744f
Roshan Limbu <topazkayle@gmail.com>
2023-05-28 09:56:52 +0545

    updated Method

--- Methods/PrintName.java
+++ Methods/PrintName.java
@@ -1,9 +1,9 @@
 // Write a program in Java to create and print your name using the method.
 public class PrintName{
   public static void main(String[]args){
-    NewFunc(); // calling function
+    namePrint(); // calling function
   }
-  static void NewFunc(){ // declaring the function here 
+  static void namePrint(){ // declaring the function here 
    System.out.println("Roshan"); // message to print
 }
 }
