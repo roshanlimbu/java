bdbec261087f81d1c9593053c53671ef8697b7b7
Roshan Limbu <topazkayle@gmail.com>
2023-05-28 10:16:40 +0545

    Intrval of even

---
+++ Methods/Interval.java
@@ -0,0 +1,11 @@
+// Write a program in Java to print even numbers between intervals using methods.
+public class Interval{
+  public static void main(String[]args){
+   evenNum(); 
+  }
+  public static void evenNum(){ // declaring the function here 
+    for(int i=6; i<=100; i+=2){
+     System.out.println(i); // message to print
+    }
+}
+}
