f1c47eef035a60d26801ee60ebf679b5c10d2fbb
Roshan Limbu <topazkayle@gmail.com>
2023-06-11 09:26:42 +0545

    updated dir

---
+++ Methods/test/AddValues.java
@@ -0,0 +1,6 @@
+public class AddValues{
+  public static void add(int x, int y){
+    int result= x+y;
+    System.out.println(result);
+  }
+}
---
+++ Methods/test/Constructor.java
@@ -0,0 +1,6 @@
+public class Constructor{
+  public static void main (String[]args){
+    AddValues calc = new AddValues();
+    calc.add(4, 5);
+  }
+}
