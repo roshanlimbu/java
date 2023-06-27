12b9780c32fb7a838f702edc4025586959ea4fe3
Roshan Limbu <topazkayle@gmail.com>
2023-06-27 10:50:53 +0545

    hierarchial

--- inheritance/Hierarchial.java
+++ inheritance/Hierarchial.java
@@ -1,1 +1,25 @@
+class Animal {
+  void eat() {
+    System.out.println("eating");
+  }
+}
 
+class Dog extends Animal {
+  void bark() {
+    System.out.println("barking");
+  }
+}
+
+class Cat extends Animal {
+  void meow() {
+    System.out.println("meowing");
+  }
+}
+
+class Hierarchial {
+  public static void main(String args[]) {
+    Cat A = new Cat();
+    A.meow();
+    A.eat();
+  }
+}
