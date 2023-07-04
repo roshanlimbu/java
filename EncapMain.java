dab4c921a206fb8c4a010558205cb5ce4b89a01d
Roshan Limbu <birenlimbu415@gmail.com>
2023-07-04 10:22:37 +0545

    getter and setter done

--- EncapMain.java
+++ EncapMain.java
@@ -2,6 +2,6 @@ public class EncapMain{
     public static void main[String[] args]{
         EncapNameGetter obj = new EncapNameGetter();
         obj.setName("Roshan");
-        System.out.println(s.getName());
+        System.out.println(obj.getName());
     }
 }
--- EncapNameGetter.java
+++ EncapNameGetter.java
@@ -1,9 +1,11 @@
 package Java;
 public EncapNameGetter{
     private String name;
+    // getter 
     public String getName(){
         return name;
     }
+    //setter
     public void setName(String name){
         this.name = name;
     }
