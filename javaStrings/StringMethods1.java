e72a4b43bf5eec47b23126e422f57b9ab1f08bf2
Roshan Limbu <topazkayle@gmail.com>
2023-07-17 09:47:30 +0545

    java strings

---
+++ javaStrings/StringMethods1.java
@@ -0,0 +1,15 @@
+public class StringMethods1{
+  public static void main(String[] args) {
+    String text = "HelloJava"; 
+    System.out.println(text.length()); // returns 10 
+    System.out.println(text.charAt(4)); // returns o 
+    System.out.println(text.substring(3)); // returns 'loJava' 
+    System.out.println(text.substring(3,6)); // returns 'loJ' 
+    String s1 = "Hello";
+    String s2 = "Java";
+    String output = s1.concat(s2); //returns HelloJava
+    System.out.println(output);
+
+  }
+
+}
