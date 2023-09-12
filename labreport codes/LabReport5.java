0b2814186d827d269cdd63a2338425a97fcc0ba3
roshanlimbu <topazkayle@gmail.com>
2023-09-12 15:19:30 +0545

    labreport5 'method overiding done'

---
+++ labreport codes/LabReport5.java
@@ -0,0 +1,42 @@
+// wap to demonstrate method overriding
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+
+    void fetch() {
+        System.out.println("Dog fetches a ball");
+    }
+}
+
+class Cat extends Animal {
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+
+    void scratch() {
+        System.out.println("Cat scratches");
+    }
+}
+
+public class LabReport5 {
+    public static void main(String[] args) {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.makeSound(); // Output: "Dog barks"
+        animal2.makeSound(); // Output: "Cat meows"
+
+        Dog dog = (Dog) animal1;
+        Cat cat = (Cat) animal2;
+        dog.fetch(); // Output: "Dog fetches a ball"
+        cat.scratch(); // Output: "Cat scratches"
+    }
+}
+
