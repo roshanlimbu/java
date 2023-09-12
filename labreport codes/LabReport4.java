de600af50b8e9cdce6f11e5fe9c9055924704545
roshanlimbu <topazkayle@gmail.com>
2023-09-12 15:43:59 +0545

    lab report 4 and 6 method overloading and constructor overloading

--- labreport codes/LabReport4.java
+++ labreport codes/LabReport4.java
@@ -1,22 +1,14 @@
-// Program to demonstrate Constructor Overloading
-public class ConstructorOverloading {
-    ConstructorOverloading() {
-        System.out.println("No-argument constructor");
-    }
 
-    ConstructorOverloading(int x, int y) {
-        int result = x + y;
-        System.out.println(result);
-    }
-
-    ConstructorOverloading(int x, int y, int z) {
-        int result = x + y + z;
-        System.out.println(result);
-    }
-
-    public static void main(String[] args) {
-        ConstructorOverloading calc0 = new ConstructorOverloading();
-        ConstructorOverloading calc1 = new ConstructorOverloading(4, 5);
-        ConstructorOverloading calc2 = new ConstructorOverloading(4, 5, 6);
-    }
+// program to demonstrate MethodOverloading
+public class MethodOverloading{
+  static void adder(int x, int y){
+    System.out.println(x+y);
+  }
+  static void adder(int x, int y, int z){
+    System.out.println(x+y+z);
+  }
+  public static void main(String[]args){
+    adder(2, 4);
+    adder(2, 4, 5);
+  }
 }
