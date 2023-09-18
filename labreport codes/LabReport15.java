cdef5ad32b1097e507486bc617ef3924718b4eba
roshanlimbu <topazkayle@gmail.com>
2023-09-18 21:23:38 +0545

    LabReport15 done

---
+++ labreport codes/LabReport15.java
@@ -0,0 +1,24 @@
+public class LabReport15 {
+  public static void main(String[] args) {
+    try {
+      int[] numbers = {1, 2, 3};
+      int result = divideNumbers(numbers, 0);
+      System.out.println("Result of division: " + result);
+    } catch (ArithmeticException e) {
+      System.out.println("An arithmetic error occurred: " + e.getMessage());
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("An array index error occurred: " + e.getMessage());
+    } finally {
+      System.out.println("Program execution completed.");
+    }
+  }
+
+  public static int divideNumbers(int[] numbers, int divisor) {
+    if (divisor == 0) {
+      throw new ArithmeticException("Division by zero is not allowed.");
+    }
+
+    return numbers[0] / divisor;
+  }
+}
+
