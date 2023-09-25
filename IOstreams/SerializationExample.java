f5364b59c3d3182049c5f981ea1cbde4d3563913
Roshan Limbu <topazkayle@gmail.com>
2023-09-25 07:55:28 +0545

    serialization and deserialization done

--- IOstreams/SerializationExample.java
+++ IOstreams/SerializationExample.java
@@ -3,40 +3,40 @@ import java.io.ObjectOutputStream;
 import java.io.IOException;
 
 public class SerializationExample {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Roshan");
-        person.setAge(21);
+  public static void main(String[] args) {
+    Person person = new Person();
+    person.setName("Roshan");
+    person.setAge(21);
 
-        try (FileOutputStream fileOut = new FileOutputStream("person.ser");
-             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+    try (FileOutputStream fileOut = new FileOutputStream("person.txt");
+    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
 
-            objectOut.writeObject(person);
-            System.out.println("Object serialized and saved to person.ser");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      objectOut.writeObject(person);
+      System.out.println("Object serialized and saved to person.txt");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
 
 class Person implements java.io.Serializable {
-    private String name;
-    private int age;
+  private String name;
+  private int age;
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 }
 
