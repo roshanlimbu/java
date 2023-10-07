470c0a304568a227e10130cdff1ca03c7ad3d7e0
Roshan Limbu <topazkayle@gmail.com>
2023-10-07 22:22:02 +0545

    constructor overloading

--- labreport codes/ConstructorOverloading.java
+++ labreport codes/ConstructorOverloading.java
@@ -1,22 +1,22 @@
 // Program to demonstrate Constructor Overloading
 public class ConstructorOverloading {
-    ConstructorOverloading() {
-        System.out.println("No-argument constructor");
-    }
+  ConstructorOverloading() {
+    System.out.println("No-argument constructor");
+  }
 
-    ConstructorOverloading(int x, int y) {
-        int result = x + y;
-        System.out.println(result);
-    }
+  ConstructorOverloading(int x, int y) {
+    int result = x + y;
+    System.out.println(result);
+  }
 
-    ConstructorOverloading(int x, int y, int z) {
-        int result = x + y + z;
-        System.out.println(result);
-    }
+  ConstructorOverloading(int x, int y, int z) {
+    int result = x + y + z;
+    System.out.println(result);
+  }
 
-    public static void main(String[] args) {
-        ConstructorOverloading calc0 = new ConstructorOverloading();
-        ConstructorOverloading calc1 = new ConstructorOverloading(4, 5);
-        ConstructorOverloading calc2 = new ConstructorOverloading(4, 5, 6);
-    }
+  public static void main(String[] args) {
+    ConstructorOverloading calc0 = new ConstructorOverloading();
+    ConstructorOverloading calc1 = new ConstructorOverloading(4, 5);
+    ConstructorOverloading calc2 = new ConstructorOverloading(4, 5, 6);
+  }
 }
